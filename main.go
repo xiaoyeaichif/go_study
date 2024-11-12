cd5b1fc74c08f0a13f828e7945dc996f94e93189
xiaoyeaichif <1125035561@qq.com>
2024-11-12 21:12:28 +0800

    三种输出的区别

--- main.go
+++ main.go
@@ -5,4 +5,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world!")
+	// 输出的区别
+	// Println在输出的结尾是带上空格的
+	// Printf/Print在输出的结尾是不带空格的
+	// 以下两句代码的输出为hello world!hello world!------》也就是说不会自动换行
+	// 换行可以通过\n来完成,也可以使用Println来是现实输出换行
+	fmt.Print("hello world!")
+	fmt.Printf("hello world!")
+
 }
